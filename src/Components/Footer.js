813d182fc56bc4170fbe9b09ef25593b99482822
mgiftson0 <mgiftson00@gmail.com>
2024-06-20 16:35:15 +0000

    Commit message

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import Logo from '../Assets/Logo.svg';
 import {BsTwitter} from 'react-icons/bs';
 import {SiLinkedin} from 'react-icons/si';
 import { BsYoutube } from 'react-icons/bs';
-import { FaFacebookF } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -16,7 +16,7 @@ const Footer = () => {
             <BsTwitter />
             <SiLinkedin />
             <BsYoutube />
-            <FaFacebookF />
+            <FaInstagram />
         </div>
       </div>
       <div className="footer-section-two">
